233487593be688be24d1c6a48229a5d14567bf19
valentino <valentino.bestak@gmail.com>
2023-01-06 21:31:07 +0100

    added timestamp to random image generator to prevent caching (showing same image in components)

--- src/util/img.tsx
+++ src/util/img.tsx
@@ -1,3 +1,3 @@
 export function getRandomImage(width = 500, height = 500): string {
-  return `https://picsum.photos/${width}/${height}`;
+  return `https://picsum.photos/${width}/${height}?t=${Date.now()}`;
 }
